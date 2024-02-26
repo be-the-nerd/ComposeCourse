96856ae3391c4558ae2c371b606fbf6394c21bb2
be-the-nerd <divyanshverma010@gmail.com>
2024-02-27 00:06:58 +0530

    Day 1 : Views in XML are equivalent to Composables in Compose. Instead of setContentView(), here we have setContent{} which can take theme and multiple composable functions.

---
+++ app/src/main/java/com/divyansh/jetpackcomposeplaylist/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.divyansh.jetpackcomposeplaylist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.divyansh.jetpackcomposeplaylist.ui.theme.JetpackComposePlaylistTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting(name = "Divyansh")
+        }
+    }
+}
+
+
+// Views(XML) = Composable(Jetpack compose)
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposePlaylistTheme {
+        Greeting("Divyansh")
+    }
+}
