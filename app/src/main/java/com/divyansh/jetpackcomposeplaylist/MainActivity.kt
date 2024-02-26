727bff446683240c47743479368f4d6b1ba5ecae
be-the-nerd <divyanshverma010@gmail.com>
2024-02-27 01:42:46 +0530

    Day 2 : Rows and Columns. Arrangement and Alignment in Row and Column. Hardcoded width and height of Row and Column.

--- app/src/main/java/com/divyansh/jetpackcomposeplaylist/MainActivity.kt
+++ app/src/main/java/com/divyansh/jetpackcomposeplaylist/MainActivity.kt
@@ -3,39 +3,64 @@ package com.divyansh.jetpackcomposeplaylist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.divyansh.jetpackcomposeplaylist.ui.theme.JetpackComposePlaylistTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Divyansh")
+
+            //Alignment and Arrangement for Row and Column
+            //Row -> horizontalArrangement and verticalAlignment
+            //Column -> horizontalAlignment and verticalArrangement
+
+            //Arrangement is Main  i.e., for Row -> Horizontal and for Column -> Vertical
+            Column {
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.5f)
+                    .background(Color.Green),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Divyansh")
+                Text("Verma")
+                Text("Android")
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .background(Color.Cyan),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+                Text("Divyansh")
+                Text("Verma")
+                Text("Android")
+            }
         }
     }
-}
-
-
-// Views(XML) = Composable(Jetpack compose)
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposePlaylistTheme {
-        Greeting("Divyansh")
     }
 }
