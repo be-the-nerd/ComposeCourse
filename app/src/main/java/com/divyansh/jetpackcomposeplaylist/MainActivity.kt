694e4fbb1949a00f2cdffc16953af0433c2c7eeb
be-the-nerd <divyanshverma010@gmail.com>
2024-02-28 01:12:46 +0530

    Day 3 : Modifiers, zoom in and zoom out, offset, border, Spacer

--- app/src/main/java/com/divyansh/jetpackcomposeplaylist/MainActivity.kt
+++ app/src/main/java/com/divyansh/jetpackcomposeplaylist/MainActivity.kt
@@ -4,63 +4,53 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.divyansh.jetpackcomposeplaylist.ui.theme.JetpackComposePlaylistTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            //Alignment and Arrangement for Row and Column
-            //Row -> horizontalArrangement and verticalAlignment
-            //Column -> horizontalAlignment and verticalArrangement
-
-            //Arrangement is Main  i.e., for Row -> Horizontal and for Column -> Vertical
-            Column {
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.5f)
-                    .background(Color.Green),
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text("Divyansh")
-                Text("Verma")
-                Text("Android")
-            }
-
             Column(
                 modifier = Modifier
+                    .background(Color.Red)
+                    .fillMaxHeight(0.5f)
                     .fillMaxWidth()
-                    .fillMaxHeight()
-                    .background(Color.Cyan),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround
+                    .padding(top = 50.dp)
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Yellow)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+//                    .width(600.dp)
+//                    .requiredWidth(600.dp)
+
+                // width -> adjust acc. to screen size
+                // requiredWidth -> does not adjust acc. to screen size and can extend to out of screen
             ) {
-                Text("Divyansh")
-                Text("Verma")
-                Text("Android")
+                Text(text = "Divyansh", modifier = Modifier.offset(50.dp, 20.dp))
+                //Offset with x and y
+                Spacer(modifier = Modifier.height(50.dp))
+                Text(
+                    text = "Verma", modifier = Modifier
+                        .border(5.dp, Color.Yellow)
+                        .padding(5.dp)
+                        .offset(20.dp, 20.dp)
+                        .border(10.dp, Color.DarkGray)
+                )
+                Text(text = "Android", modifier = Modifier.clickable { })
             }
         }
-    }
     }
 }
